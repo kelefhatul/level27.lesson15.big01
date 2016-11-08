c61fdf1e58e18c0a469457b62561c192f4c6c537
kelefhatul <kostyantinromanov@gmail.com>
2016-11-08 11:18:15 +0200

    task08

--- big01/Tablet.java
+++ big01/Tablet.java
@@ -1,5 +1,6 @@
 package com.javarush.test.level27.lesson15.big01;
 
+import com.javarush.test.level27.lesson15.big01.ad.AdvertisementManager;
 import com.javarush.test.level27.lesson15.big01.kitchen.Order;
 
 import java.io.IOException;
@@ -22,10 +23,11 @@ public class Tablet extends Observable {
         try {
             order = new Order(this);
             if (!order.isEmpty()){
-            ConsoleHelper.writeMessage(order.toString());
-            setChanged();
-            notifyObservers(order);
+                ConsoleHelper.writeMessage(order.toString());
+                setChanged();
+                notifyObservers(order);
             }
+            new AdvertisementManager(order.getTotalCookingTime()).processVideos();
         } catch (IOException ioe) {
             logger.log(Level.SEVERE, "Console is unavailable.");
         }
--- big01/ad/AdvertisementManager.java
+++ big01/ad/AdvertisementManager.java
@@ -1,8 +1,18 @@
 package com.javarush.test.level27.lesson15.big01.ad;
 
+import com.javarush.test.level27.lesson15.big01.ConsoleHelper;
+
 /**
  * Created by root on 11/7/2016.
  */
 public class AdvertisementManager {
+    private int timeSeconds;
     private final AdvertisementStorage storage = AdvertisementStorage.getInstance();
+
+    public AdvertisementManager(int timeSeconds) {
+        this.timeSeconds = timeSeconds;
+    }
+    public void processVideos(){
+        ConsoleHelper.writeMessage("processVideos method calling");
+    }
 }
