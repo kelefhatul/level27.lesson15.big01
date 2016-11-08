09328f9bc82bb07e2e09b47bdf19006870710d8e
kelefhatul <kostyantinromanov@gmail.com>
2016-11-08 11:01:35 +0200

    task07

--- big01/ad/AdvertisementStorage.java
+++ big01/ad/AdvertisementStorage.java
@@ -1,16 +1,38 @@
 package com.javarush.test.level27.lesson15.big01.ad;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by root on 11/7/2016.
  */
-public class AdvertisementStorage {
+class AdvertisementStorage {
+
     private static AdvertisementStorage instance;
-    private AdvertisementStorage(){}
-    public static synchronized AdvertisementStorage getInstance(){
+    private final List<Advertisement> videos = new ArrayList<>();
+
+    private AdvertisementStorage(){
+        Object someContent = new Object();
+        add(new Advertisement(someContent, "First Video", 5000, 100, 3 * 60)); // 3 min
+        add(new Advertisement(someContent, "Second Video", 100, 10, 15 * 60)); //15 min
+        add(new Advertisement(someContent, "Third Video", 400, 2, 10 * 60));   //10 min
+    }
+
+    public static AdvertisementStorage getInstance(){
         if (instance == null){
             instance = new AdvertisementStorage();
         }
         return instance;
     }
 
+    public List<Advertisement> list() {
+        return videos;
+    }
+
+    public void add(Advertisement advertisement){
+        if (advertisement!=null) {
+            videos.add(advertisement);
+        }
+    }
 }
